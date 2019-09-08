3644a096db9aabee09adc050a64e57f6541e95f6
anovus <marwin.quito@gmail.com>
2019-09-07 23:58:13 -0500

    Added file via upload

---
+++ FileTypeMove.ps1
@@ -0,0 +1,23 @@
+﻿$ext= Read-Host -Prompt 'Enter File Type extension (example: .txt, .xls)'
+
+$sourceDir = Read-Host -Prompt 'Enter Source Directory that you want to move the files from'
+
+$targetDir = Read-Host -Prompt 'Enter Target Directory that you want to move files to'
+
+$pathExists = Test-Path $targetDir
+
+If ($pathExists) {
+
+Write-Output 'Target Directory already exists, moving files..'
+
+}
+
+Else {
+
+Write-Output 'Target Directory does not exist. Creating directory and moving files...'
+
+New-Item -ItemType "directory" -Path $targetDir
+
+}
+
+Move-Item -Path $sourceDir\*$ext -Destination $targetDir
